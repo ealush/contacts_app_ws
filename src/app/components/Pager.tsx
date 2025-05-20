33e49e7331db419e41a68c883ce8f97f24df72a6
Evyatar Alush <ealush@meta.com>
2025-05-20 23:31:47 +0300

    10_pager__hooks_send_temporary_message_with_useOptimistic

--- src/app/components/Pager.tsx
+++ src/app/components/Pager.tsx
@@ -5,16 +5,19 @@ import React, {
   useEffect,
   useActionState,
   useTransition,
+  useOptimistic,
 } from "react";
 import styles from "./pager.module.css";
 import sendMessageAction from "../actions/sendMessageAction";
 import fetchMessagesAction from "../actions/fetchMessagesAction";
+import clsx from "clsx";
 
 interface Message {
   id: number;
   content: string;
   timestamp: string;
   contactId: number;
+  isOptimistic?: boolean;
 }
 
 interface PagerProps {
@@ -25,6 +28,13 @@ export default function Pager({ contactId }: PagerProps) {
   const [, formAction, isPending] = useActionState(sendMessage, null);
   const [messages, setMessages] = useState<Message[]>([]);
   const [isLoading, startTransition] = useTransition();
+  const [optimisticMessages, addOptimisticMessage] = useOptimistic(
+    messages,
+    (prevMessages: Message[], newMessage: Message) => [
+      newMessage,
+      ...prevMessages,
+    ]
+  );
 
   useEffect(() => {
     startTransition(async () => {
@@ -45,8 +55,13 @@ export default function Pager({ contactId }: PagerProps) {
       <div className={styles.messageList}>
         {isLoading && <p>Loading messages...</p>}
         {!isLoading &&
-          messages.map((msg) => (
-            <div key={msg.id} className={styles.messageItem}>
+          optimisticMessages.slice(0, 5).map((msg) => (
+            <div
+              key={msg.id}
+              className={clsx(styles.messageItem, {
+                [styles.optimistic]: msg.isOptimistic,
+              })}
+            >
               <p className={styles.messageContent}>{msg.content}</p>
               <span className={styles.messageTimestamp}>
                 {formatTimestamp(msg.timestamp)}
@@ -62,6 +77,7 @@ export default function Pager({ contactId }: PagerProps) {
           className={styles.messageInput}
           aria-label="New message input"
           disabled={isLoading}
+          name="content"
         />
         <button
           type="submit"
@@ -75,6 +91,14 @@ export default function Pager({ contactId }: PagerProps) {
   );
 
   async function sendMessage(_: unknown, formData: FormData) {
+    addOptimisticMessage({
+      id: Date.now(),
+      content: formData.get("content") as string,
+      timestamp: new Date().toISOString(),
+      contactId: contactId,
+      isOptimistic: true,
+    });
+
     const message = await sendMessageAction(formData);
 
     setMessages((prevMessages) => [message, ...prevMessages]);
