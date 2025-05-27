94d0e66492dc412d3b3042d6ca1bdcde71c8e6bb
Evyatar Alush <ealush@meta.com>
2025-05-27 23:36:38 +0300

    01_done

--- src/app/components/ButtonFavorite.tsx
+++ src/app/components/ButtonFavorite.tsx
@@ -3,18 +3,30 @@
 import { FaRegStar, FaStar } from "react-icons/fa";
 import { ContactWithFavorite } from "../types";
 import styles from "./Contact.module.css";
+import { useRouter } from "next/navigation";
 
 type ContactProps = {
   contact: ContactWithFavorite;
 };
 
 export function ButtonFavorite({ contact }: ContactProps) {
+  const router = useRouter();
+
   return (
     <button
       className={styles.actionButton}
+      onClick={toggleFavorite}
       title={contact.isFavorite ? "Remove from favorites" : "Add to favorites"}
     >
       {contact.isFavorite ? <FaStar /> : <FaRegStar />}
     </button>
   );
+
+  async function toggleFavorite() {
+    await fetch(`/api/contacts/${contact.id}/favorite`, {
+      method: "POST",
+    });
+
+    router.refresh();
+  }
 }
