90ca01bf1c653b49e9550d35c936a1fed88a3e78
Evyatar Alush <ealush@meta.com>
2025-05-28 16:13:03 +0300

    14_done

--- src/app/validations/contactFormSuite.ts
+++ src/app/validations/contactFormSuite.ts
@@ -1,8 +1,10 @@
 import { create, test, enforce, only } from "vest";
+import doesEmailExist from "../db/doesEmailExist";
 
 export const contactFormSuite = create(
   "contactFormSuite",
   (data: FormData, currentField: string) => {
+    const id = data.get("id") as string;
     const firstName = data.get("firstName") as string;
     const lastName = data.get("lastName") as string;
     const email = data.get("email") as string;
@@ -18,5 +20,19 @@ export const contactFormSuite = create(
     test("email", "Email is required", () => {
       enforce(email).isNotBlank();
     });
+
+    test.memo(
+      "email",
+      "email already exists",
+      async () => {
+        const exists = await doesEmailExist(
+          email,
+          id ? parseInt(id) : undefined
+        );
+
+        enforce(exists).isFalsy();
+      },
+      [email, id]
+    );
   }
 );
