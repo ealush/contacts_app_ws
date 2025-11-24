d97daee5115cbfa11a5132746755a4029a95dbde
Evyatar Alush <ealush@meta.com>
2025-11-25 01:15:57 +0200

    starter

--- src/app/components/ButtonFavorite.tsx
+++ src/app/components/ButtonFavorite.tsx
@@ -1,33 +1,31 @@
 "use client";
 
 import { FaRegStar, FaStar } from "react-icons/fa";
-// import { ContactWithFavorite } from "../types";
+import { ContactWithFavorite } from "../types";
 import styles from "./Contact.module.css";
-import { useState } from "react";
+// import { useRouter } from "next/navigation";
 
-// type ContactProps = {
-//   contact: ContactWithFavorite;
-// };
+type ContactProps = {
+  contact: ContactWithFavorite;
+};
 
-export function ButtonFavorite(/*{ contact }: ContactProps*/) {
-  const [isFavorite /*setIsFavorite*/] =
-    useState(/* contact.isFavorite?.contactId === contact.id */);
+export function ButtonFavorite({ contact }: ContactProps) {
+  // const router = useRouter();
 
   return (
     <button
       className={styles.actionButton}
-      // onClick={toggleFavorite}
-      title={isFavorite ? "Remove from favorites" : "Add to favorites"}
+      onClick={toggleFavorite}
+      title={contact.isFavorite ? "Remove from favorites" : "Add to favorites"}
     >
-      {isFavorite ? <FaStar /> : <FaRegStar />}
+      {contact.isFavorite ? <FaStar /> : <FaRegStar />}
     </button>
   );
 
-  // async function toggleFavorite() {
-  //   await fetch(`/api/contacts/${contact.id}/favorite`, {
-  //     method: "POST",
-  //   });
-
-  //   setIsFavorite((prev) => !prev);
-  // }
+  async function toggleFavorite() {
+    // await fetch(`/api/contacts/${contact.id}/favorite`, {
+    //   method: "POST",
+    // });
+    // router.refresh();
+  }
 }
